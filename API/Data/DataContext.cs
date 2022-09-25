b54ee62a39dfc08260b90a84220f4fd8b5027038
robert-lupoaie <robert.lupoaie@gep.com>
2022-09-26 01:50:41 +0300

    End of section 8

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -9,9 +9,11 @@ namespace API.Data
 {
     public class DataContext : DbContext
     {
-        public DbSet<AppUser> Users { get; set; }
         public DataContext(DbContextOptions options) : base(options)
         {
         }
+
+        public DbSet<AppUser> Users { get; set; }
+        
     }
 }
