278fe52c8ff68af2594b8c8367cb9602b4dc75c3
robert-lupoaie <robert.lupoaie@gep.com>
2022-10-15 14:07:37 +0300

    End of section 9

---
+++ client/src/app/_interceptors/jwt.interceptor.ts
@@ -0,0 +1,30 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
+import { User } from '../_models/user';
+import { AccountService } from '../_services/account.service';
+
+
+@Injectable()
+export class JWtInterceptor implements HttpInterceptor {
+
+  constructor(private accountService: AccountService) { }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    let currentUser: User;
+
+    this.accountService.currentUser$.pipe(take(1)).subscribe(user => currentUser = user);
+
+    if (currentUser) {
+      req = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${currentUser.token}`
+        }
+      });
+    }
+
+    return next.handle(req);
+  }
+
+}
--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { ToastrModule } from 'ngx-toastr';
-
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
@@ -10,11 +11,15 @@ import { ToastrModule } from 'ngx-toastr';
   imports: [
     CommonModule,
     BsDropdownModule.forRoot(),
-    ToastrModule.forRoot({ positionClass: 'toast-bottom-right' })
+    ToastrModule.forRoot({ positionClass: 'toast-bottom-right' }),
+    TabsModule.forRoot(),
+    HttpClientModule,
   ],
   exports: [
     BsDropdownModule,
-    ToastrModule
+    ToastrModule,
+    TabsModule,
+    HttpClientModule
   ]
 })
 export class SharedModule { }
